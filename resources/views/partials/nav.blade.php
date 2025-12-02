25372634ce124eda2f36526fd1123f04101bb666
Baboo81 <magicbaboo@gmail.com>
2025-12-02 14:54:48 +0100

    navbar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -4,23 +4,25 @@
     <!-- Sidebar -->
     <nav id="sidebar" class="js-fullheight">
         <h1>
-            <a href="{{ url('/') }}" class="logo">M.</a>
+            <a href="{{ url('/') }}" class="logo">
+                <img src="{{ asset('assets/img/logo/logo.svg') }}" alt="logo" class="sidebar-logo">
+            </a>
         </h1>
         <ul class="list-unstyled components mb-5">
             <li class="active">
-                <a href="{{ url('/') }}"><span class="fa fa-home"></span> Home</a>
+                <a href="{{ url('/') }}"><span class="fa fa-home mx-1"></span>Accueil</a>
             </li>
             <li>
-                <a href="{{ url('/about') }}"><span class="fa fa-user"></span> About</a>
+                <a href="{{ url('/about') }}"><span class="fa fa-user mx-1"></span>À propos</a>
             </li>
             <li>
-                <a href="{{ url('/blog') }}"><span class="fa fa-sticky-note"></span> Blog</a>
+                <a href="{{ url('/blog') }}"><span class="fa fa-sticky-note mx-1"></span>Services</a>
             </li>
             <li>
-                <a href="{{ url('/services') }}"><span class="fa fa-cogs"></span> Services</a>
+                <a href="{{ url('/services') }}"><span class="fa fa-cogs mx-1"></span>Outils</a>
             </li>
             <li>
-                <a href="{{ url('/contact') }}"><span class="fa fa-paper-plane"></span> Contact</a>
+                <a href="{{ url('/contact') }}"><span class="fa fa-paper-plane mx-1"></span>Contact</a>
             </li>
         </ul>
     </nav>
